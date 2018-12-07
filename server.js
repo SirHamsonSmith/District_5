a69d7224dbb09e1c82838105f865eb35bd4a9516
mansouriamn <38742344+mansouriamn@users.noreply.github.com>
2018-12-06 18:25:53 -0800

    This is thelast changed version

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const bodyparser = require('body-parser');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'Views')))
 app.use(bodyparser.urlencoded({
     extended: true
 }))
 app.use(bodyparser.json())
 
-require('./routes/routes')(app)
+require('./Controllers/routesController')(app)
 
-require('./models').sequelize.sync().then(() => app.listen(3200, () => console.log('http://localhost:3200')))
+require('./Models').sequelize.sync().then(() => app.listen(3200, () => console.log('http://localhost:3200')))
